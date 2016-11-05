0a03990f7439a935d3ad3354eb690f472631b550
henyxia <henyxia@live.fr>
2016-11-05 01:14:10 +0100

    Small diff, big impact, should not work properly

--- input.c
+++ input.c
@@ -1,1 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdbool.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+
 #include "input.h"
+
+// Private function
+void inputProcess(commonData*);
+
+// Public function
+void* input_thread(void* comm_raw)
+{
+	commonData* comm = comm_raw;
+
+	inputProcess(comm);
+
+	comm->threadStarted--;
+
+	return NULL;
+}
+
+void inputProcess(commonData* comm)
+{
+	// Vars
+	bool			stop = false;
+	fd_set			rfds;
+	struct termios	old = { 0 };
+	int				max;
+	char			buffer[MAX_MSG_LEN];
+	int				size;
+
+	// Setting the terminal properly
+    if(tcgetattr(0, &old)<0)
+        perror("tcsetattr()");
+    old.c_lflag&=~ICANON;
+   	old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN]=1;
+    old.c_cc[VTIME]=0;
+    if(tcsetattr(0, TCSANOW, &old)<0)
+        perror("tcsetattr ICANON");
+
+	FD_ZERO(&rfds);
+	FD_SET(STDIN_FILENO, &rfds);
+	max = 1;
+
+	while(!stop)
+	{
+		FD_SET(STDIN_FILENO, &rfds);
+
+		select(max, &rfds, NULL, NULL, NULL);
+
+		if(FD_ISSET(STDIN_FILENO, &rfds))
+		{
+			size = read(STDIN_FILENO, buffer, MAX_MSG_LEN);
+			buffer[size] = 0;
+			printfd(comm->fd_status, "> lol %s", buffer);
+			//printf("\x1b[%d;2H Stdin ? %c", comm->hsize-1, buffer[0]);
+			//fflush(stdout);
+			//completeStatusBar(comm->wsize, size+8);
+		}
+		else
+		{
+			// WTF ? We should never be here
+			fprintf(stderr, "PANIC... Unexpected behavior\n");
+		}
+
+		FD_ZERO(&rfds);
+	}
+}
